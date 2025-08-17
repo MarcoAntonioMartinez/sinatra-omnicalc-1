38f2a91b88e4c8905371526f1ae925911967c6ee
Marco Martinez <marco.antoni.martinez@gmail.com>
2025-08-17 23:18:45 +0000

    Added payment views

--- app.rb
+++ app.rb
@@ -1,5 +1,6 @@
 require "sinatra"
 require "sinatra/reloader"
+require "active_support/all"
 
 get("/") do
   erb(:home)
@@ -28,11 +29,43 @@ get("/square_root/new") do
 end
 
 get("/square_root/result") do
-
+#maybe put round to get the float zero out or something idk
+  #@number = params.fetch("number").to_f.to_fs(:percentage, {:precision => 0})
   @number = params.fetch("number").to_i
 
   @result = Math.sqrt(@number) * 1.0
 
   erb(:sqrt)
 
+end
+
+get("/payment/new") do
+  erb(:payment_new)
+end
+
+
+
+
+get("/payment/result") do
+
+  @apr = params.fetch("apr").to_f.to_fs(:percentage, {:precision => 4})
+  @years = params.fetch("years").to_i
+
+  @principal = params.fetch("principal").to_f
+
+  @r = @apr.to_f/ 100 / 12
+
+  @n = 12 * @years
+
+  @numerator = @r * @principal
+
+  @denominator = 1 - (1 + @r) ** -@n
+
+  @payment = @numerator / @denominator
+
+  @principal = @principal.to_fs(:currency)
+
+  @payment = @payment.to_f.to_fs(:currency)
+
+  erb(:payment)
 end
