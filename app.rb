288d88ad13d18292a458ad3d72ae9ab06a4a82fc
Marco Martinez <marco.antoni.martinez@gmail.com>
2025-08-17 22:20:36 +0000

    Added square and square root routes and views

--- app.rb
+++ app.rb
@@ -4,3 +4,35 @@ require "sinatra/reloader"
 get("/") do
   erb(:home)
 end
+
+get("/square/result") do
+
+  @number = params.fetch("number").to_i
+
+  @result = @number * @number.to_f
+
+  erb(:square)
+
+end
+
+get("/square/new") do 
+
+  erb(:home)
+
+end
+
+get("/square_root/new") do
+
+  erb(:sqrt_new)
+
+end
+
+get("/square_root/result") do
+
+  @number = params.fetch("number").to_i
+
+  @result = Math.sqrt(@number) * 1.0
+
+  erb(:sqrt)
+
+end
